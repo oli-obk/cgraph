b5c25a2a5091f020ff84b65bb6a67bba9f07adef
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2021-03-06 22:11:14 +0000

    Also look in Graphviz/lib/
    This fixed building priroda on windows for me
    
    However, I still needed the Graphviz/bin in my PATH, which doesn't quite
    seem right since the last commit claimed to use static
    linking
    
    I've also rustfmted, kind of accidentally
    
    It's possible that this change is doing nothing, and something else
    is breaking static linking

--- build.rs
+++ build.rs
@@ -2,10 +2,7 @@ use std::fs::read_dir;
 
 fn main() {
     let dirs = read_dir("C:\\Program Files (x86)\\")
-        .and_then(|dirs| {
-            read_dir("C:\\Program Files\\")
-                .map(|dirs2| dirs.chain(dirs2))
-        });
+        .and_then(|dirs| read_dir("C:\\Program Files\\").map(|dirs2| dirs.chain(dirs2)));
     if let Ok(dirs) = dirs {
         for entry in dirs {
             if let Ok(entry) = entry {
@@ -13,6 +10,7 @@ fn main() {
                     if file.starts_with("Graphviz") {
                         let path = entry.path();
                         if let Some(path) = path.to_str() {
+                            println!("cargo:rustc-link-search={}\\lib", path);
                             println!("cargo:rustc-link-search={}\\lib\\debug\\lib", path);
                         }
                     }
