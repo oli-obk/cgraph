7348380f014fe87338cdcd4d2f22cbda771ae21d
Oli Scherer <github35764891676564198441@oli-obk.de>
2021-03-10 14:37:14 +0000

    Try using pkg-config

--- build.rs
+++ build.rs
@@ -1,21 +1,32 @@
 use std::fs::read_dir;
 
 fn main() {
-    let dirs = read_dir("C:\\Program Files (x86)\\")
-        .and_then(|dirs| read_dir("C:\\Program Files\\").map(|dirs2| dirs.chain(dirs2)));
-    if let Ok(dirs) = dirs {
-        for entry in dirs {
-            if let Ok(entry) = entry {
-                if let Some(file) = entry.file_name().to_str() {
-                    if file.starts_with("Graphviz") {
-                        let path = entry.path();
-                        if let Some(path) = path.to_str() {
-                            println!("cargo:rustc-link-search={}\\lib", path);
-                            println!("cargo:rustc-link-search={}\\lib\\debug\\lib", path);
+    if cfg!(windows) {
+        let dirs = read_dir("C:\\Program Files (x86)\\")
+            .and_then(|dirs| read_dir("C:\\Program Files\\").map(|dirs2| dirs.chain(dirs2)));
+        if let Ok(dirs) = dirs {
+            for entry in dirs {
+                if let Ok(entry) = entry {
+                    if let Some(file) = entry.file_name().to_str() {
+                        if file.starts_with("Graphviz") {
+                            let path = entry.path();
+                            if let Some(path) = path.to_str() {
+                                println!("cargo:rustc-link-search={}\\lib", path);
+                                println!("cargo:rustc-link-search={}\\lib\\debug\\lib", path);
+                            }
                         }
                     }
                 }
             }
         }
+    } else {
+        pkg_config::Config::new()
+            .atleast_version("2.0")
+            .probe("libcgraph")
+            .unwrap();
+        pkg_config::Config::new()
+            .atleast_version("2.0")
+            .probe("libgvc")
+            .unwrap();
     }
 }
